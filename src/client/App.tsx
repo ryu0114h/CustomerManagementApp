8eb6e28e491d19ce13814651f5a8fb6255db6603
ryu0114h <ryu0114h@gmail.com>
2021-02-08 17:40:19 +0900

    file構成変更

---
+++ src/client/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <p>練習中...</p>
+      <p>練習中...</p>
+      <p>練習中...</p>
+    </div>
+  );
+};
+
+export default App;
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,15 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const App = () => {
-  return (
-    <div>
-      <h1>Hello React</h1>
-      <p>練習中...</p>
-      <p>練習中...</p>
-      <p>練習中...</p>
-    </div>
-  );
-};
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
