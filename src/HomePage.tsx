d40176fb36f49a210fa1f32aa0295b5aa1bd75f1
ryu0114h <ryu0114h@gmail.com>
2021-02-14 11:23:52 +0900

    create HomePage.tsx

---
+++ src/HomePage.tsx
@@ -0,0 +1,270 @@
+import React from "react";
+import { Table, Tag } from "antd";
+import "antd/dist/antd.css";
+
+const { Column, ColumnGroup } = Table;
+
+const data = [
+  {
+    key: "1",
+    firstName: "John",
+    lastName: "Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    tags: ["nice", "developer"],
+  },
+  {
+    key: "2",
+    firstName: "Jim",
+    lastName: "Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+    tags: ["loser"],
+  },
+  {
+    key: "3",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "4",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "5",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "6",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "7",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "8",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "9",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "10",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "11",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "12",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "13",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "14",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "15",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "16",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "17",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "18",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "19",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "20",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "21",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "22",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "23",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "24",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "25",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "26",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "27",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "28",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+  {
+    key: "29",
+    firstName: "Joe",
+    lastName: "Black",
+    age: 32,
+    address: "Sidney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+];
+
+const HomePage: React.FC = () => {
+  return (
+    <Table dataSource={data} style={{ width: "80%", margin: "100px auto" }}>
+      <Column title="番号" dataIndex="key" key="key" />
+      <ColumnGroup title="名前">
+        <Column title="性" dataIndex="lastName" key="lastName" />
+        <Column title="名" dataIndex="firstName" key="firstName" />
+      </ColumnGroup>
+      <Column title="年齢" dataIndex="age" key="age" />
+      <Column title="住所" dataIndex="address" key="address" />
+      <Column
+        title="タグ"
+        dataIndex="tags"
+        key="tags"
+        render={(tags) => (
+          <>
+            {tags.map((tag) => (
+              <Tag color="blue" key={tag}>
+                {tag}
+              </Tag>
+            ))}
+          </>
+        )}
+      />
+    </Table>
+  );
+};
+
+export default HomePage;
