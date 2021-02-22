33f8e6b4eee0985472e12650f5304a20c25362ba
ryu0114h <ryu0114h@gmail.com>
2021-02-23 03:26:18 +0900

    create signup_page

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,12 +4,14 @@ import HomePage from "./pages/HomePage";
 import AddPage from "./pages/AddPage";
 import DetailPage from "./pages/DetailPage";
 import EditPage from "./pages/EditPage";
+import SignUp from "./pages/SignUp";
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/signup" component={SignUp} />
         <Route path="/add" component={AddPage} />
 
         <Route exact path="/:id" component={DetailPage} />
