33ffd0fcc16f7a78762e2991e9871989d9750387
ryu0114h <ryu0114h@gmail.com>
2021-02-25 03:28:20 +0900

    redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
-import configureStore from "./reducks/store/store";
+import { configureStore, persistor } from "./reducks/store/store";
 
-const store = configureStore();
+const store = configureStore;
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
