ea1280059a97e09f1f4b7b49d6e62a590f70626c
ryu0114h <ryu0114h@gmail.com>
2021-02-24 18:13:19 +0900

    add to Auth

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,18 +6,21 @@ import DetailPage from "./pages/DetailPage";
 import EditPage from "./pages/EditPage";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import Auth from "./Auth";
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={HomePage} />
         <Route path="/signup" component={SignUp} />
         <Route path="/signin" component={SignIn} />
-        <Route path="/add" component={AddPage} />
 
-        <Route exact path="/:id" component={DetailPage} />
-        <Route path="/:id/edit" component={EditPage} />
+        <Auth>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/add" component={AddPage} />
+          <Route exact path="/:id" component={DetailPage} />
+          <Route path="/:id/edit" component={EditPage} />
+        </Auth>
       </Switch>
     </BrowserRouter>
   );
